66723a7593f0c0dcdc789a4408aa650f7817d9db
Alexandra Lazarova <aleksandra.g.lazarova.2020@elsys-bg.org>
2025-03-21 01:53:16 +0200

    added id

--- ble/ble_advertisement/ble_advertisement.ino
+++ ble/ble_advertisement/ble_advertisement.ino
@@ -3,8 +3,7 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-#define DEVICE_NAME "M5Core2_Fall_Detect"
-#define DEVICE_ID "123456" // Unique ID for this device
+#define DEVICE_NAME "M5Core2_Fall_Detect1"
 
 BLEAdvertising* pAdvertising;
 
@@ -19,11 +18,8 @@ void setup() {
 
     pAdvertising = BLEDevice::getAdvertising();
     
-    // Set BLE advertisement data
     BLEAdvertisementData advData;
     advData.setName(DEVICE_NAME);
-    String manufacturerData = "ID:" + String(DEVICE_ID); // Format manufacturer data
-    advData.setManufacturerData(manufacturerData);
 
     pAdvertising->setAdvertisementData(advData);
     pAdvertising->start();
@@ -34,7 +30,6 @@ void setup() {
     M5.Lcd.setCursor(10, 80);
     M5.Lcd.print("Device: " DEVICE_NAME);
     M5.Lcd.setCursor(10, 110);
-    M5.Lcd.print("ID: " DEVICE_ID);
 }
 
 void loop() {
@@ -54,7 +49,6 @@ void loop() {
         M5.Lcd.println("BLE Advertising!");
         M5.Lcd.print("Device: " DEVICE_NAME);
         M5.Lcd.setCursor(10, 110);
-        M5.Lcd.print("ID: " DEVICE_ID);
     }
 
     delay(500);
--- ble/ble_receive_message/ble_receive_message.ino
+++ ble/ble_receive_message/ble_receive_message.ino
@@ -6,29 +6,38 @@
 #define SCAN_TIME 5
 #define TARGET_DEVICE "M5Core2_Fall_Detect"
 
-BLEScan* pBLEScan;
+BLEScan *pBLEScan;
 
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-    void onResult(BLEAdvertisedDevice advertisedDevice) {
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+    void onResult(BLEAdvertisedDevice advertisedDevice)
+    {
         String deviceName = advertisedDevice.getName().c_str();
         String target_device = TARGET_DEVICE;
         int target_device_length = target_device.length();
 
-        for (int i = 0; i < target_device_length; i++) {
-            if (deviceName[i] != target_device[i]) {
+        for (int i = 0; i < target_device_length; i++)
+        {
+            if (deviceName[i] != target_device[i])
+            {
                 return;
             }
         }
 
         String id = deviceName.substring(target_device_length);
 
-        Serial.println("🔵 M5Core2 Detected!");
-        Serial.print("📡 RSSI: "); Serial.println(advertisedDevice.getRSSI());
-        
-        if (advertisedDevice.haveManufacturerData()) {
+        Serial.println("M5Core2 Detected!");
+        Serial.print("RSSI: ");
+        Serial.println(advertisedDevice.getRSSI());
+        Serial.print("ID: ");
+        Serial.println(id);
+
+        if (advertisedDevice.haveManufacturerData())
+        {
             String manufacturerData = advertisedDevice.getManufacturerData();
-            Serial.print("🏭 Manufacturer Data: ");
-            for (char c : manufacturerData) Serial.print(c);
+            Serial.print("Manufacturer Data: ");
+            for (char c : manufacturerData)
+                Serial.print(c);
             Serial.println();
         }
 
@@ -36,20 +45,22 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     }
 };
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    Serial.println("\n🔍 BLE Scanner Started");
+    Serial.println("\nBLE Scanner Started");
 
     BLEDevice::init("ESP32S3_Receiver");
     pBLEScan = BLEDevice::getScan();
     pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-    pBLEScan->setActiveScan(true); 
+    pBLEScan->setActiveScan(true);
     pBLEScan->setInterval(100);
     pBLEScan->setWindow(99);
 }
 
-void loop() {
-    Serial.println("🔄 Scanning...");
+void loop()
+{
+    Serial.println("Scanning...");
     pBLEScan->start(SCAN_TIME, false);
-    delay(1000); 
+    delay(1000);
 }
